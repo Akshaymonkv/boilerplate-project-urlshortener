581f5f88eb858ced97a482ee42d6a69450a7237c
Akshaymon K V <kvakshaymon@gmail.com>
2024-10-26 21:08:07 +0530

    Made changes to extract the post data and validate if it is a valid dns

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+const dns = require('dns');
+const url = require('url');
+const RandomNumber = require('random_numbers_generator')
+const { hostname } = require('os');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +25,33 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl',(req,res)=>{
+ const {body} = req
+ //console.log(body.url)
+ const parsedUrl = url.parse(body.url)
+ //console.log(parsedUrl.hostname)
+ //checking if url is valid
+ dns.resolve(parsedUrl.hostname,(err)=>{
+  if(err){res.json({error: 'Invalid URL'})}
+  //if valid
+  else{
+    const extUrlValue = randomNumgen()
+     
+     res.json({
+      original_url: body.url,
+      short_url: extUrlValue
+     })
+     
+  }
+ })
+  }
+)
+
+function randomNumgen(){
+  const random_num = RandomNumber.getRandomNumber(1,100)
+  return random_num
+}
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
